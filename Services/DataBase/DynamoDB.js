9c66c43d742af3112b7fe2773b96d5625c53c08e
Henrique <henriquecruz@brasilcap.com.br>
2018-05-07 15:51:13 -0300

    Criação do DynamoDB para ser utilizado

---
+++ Services/DataBase/DynamoDB.js
@@ -0,0 +1,59 @@
+var AWS = require("aws-sdk");
+
+var docClient;
+
+module.exports = class DynamoDB {
+
+    constructor() {
+        docClient = new AWS.DynamoDB.DocumentClient();
+    }
+
+    delete(tableName, keyName, key, callback) {
+        var object = {
+            TableName: tableName,
+            Key: {
+                [keyName]: key
+            }
+        };
+
+        docClient.delete(object, function (err, data) {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, { message: "Deleted successfully" });
+            }
+        });
+    }
+
+    get(tableName, keyName, key, callback) {
+        var object = {
+            TableName: tableName,
+            Key: {
+                [keyName]: key
+            }
+        };
+
+        docClient.get(object, function (err, data) {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, data.Item);
+            }
+        });
+    }
+
+    put(tableName, item, callback) {
+        var object = {
+            TableName: tableName,
+            Item: item
+        };
+
+        docClient.put(object, function (err, data) {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, data);
+            }
+        });
+    }
+}
