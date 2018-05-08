1bd80195680dc45466d4f2ab55be5d4c5e1776be
Henrique <henriquecruz@brasilcap.com.br>
2018-05-08 17:49:08 -0300

    Fork com o brcap-aws

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ exports.SNS_Post = function (snsURL, payload, subject, region, callback) {
         callback(err, data);
     });
 }
+
 exports.SQS_Get = function (queueURL, region, callback) {
     SQS_Get = new SQS(region);
 
