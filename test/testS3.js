2debf91f2c050d0c91b0f9d6dc48ee6fb3f1c0b9
Welington Cruz de Assis <welingtoncassis@gmail.com>
2020-12-07 17:44:40 -0300

    feat(dependencies): add jest and upgrade aws-sdk

--- test/testS3.js
+++
@@ -1,9 +0,0 @@
-var BRCAPAWS = require('../index.js');
-
-BRCAPAWS.S3_Get('bucket', 'file', function(err, data){
-    if (err){
-        console.log(err);
-    }else{
-        console.log(data);
-    }
-});
---
+++ test/testS3.test.js
@@ -0,0 +1,7 @@
+const BRCAPAWS = require('../index.js');
+
+describe('API S3', function() {
+    test('Initialize S3', function(){
+        
+    })
+})
